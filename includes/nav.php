eaef6f45bfebf6e447ac26e71b87f0f5cc270142
Deepa Hiremath <deepa.h@pixafly.in>
2019-07-30 15:11:47 +0530

    Created Navigation

--- includes/nav.php
+++ includes/nav.php
@@ -1,4 +1,31 @@
-
-    <header>
-      
-    </header>
+<nav class="navbar navbar-expand-lg">
+  <div class="container">
+    <a class="navbar-brand" href="javascript:void(0);">Navbar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="javascript:void(0);navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="javascript:void(0);">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="javascript:void(0);">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="javascript:void(0);">Pricing</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="javascript:void(0);" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Dropdown link
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <a class="dropdown-item" href="javascript:void(0);">Action</a>
+            <a class="dropdown-item" href="javascript:void(0);">Another action</a>
+            <a class="dropdown-item" href="javascript:void(0);">Something else here</a>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
